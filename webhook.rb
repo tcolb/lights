5669d04c186697c7499081bd466d08ab6b0dba77
Tristan Colby <tristan.colb@gmail.com>
2020-05-21 12:51:13 +0100

    added sinatra webhook

---
+++ webhook.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+
+require_relative 'webhooksecret'
+
+set :port, 3254
+
+post '/payload' do
+  request.body.rewind
+  payload_body = request.body.read
+  if verify_signature(payload_body)
+    puts "Webhook signature matches, pulling from git..."
+    puts `git pull`
+  end
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), WebhookSecret.secret, payload_body)
+  return false, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+  return true
+end
