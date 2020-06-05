5ab93b93dafe8e86f1e5b8d3b51401cad39d0543
Tristan Colby <tristan.colb@gmail.com>
2020-06-05 04:34:52 +0100

    refactored into two services

--- start_webhook.sh
+++ start_webhook.sh
@@ -1,4 +1,3 @@
 export LIGHTSIDENTITY=him
-/home/pi/lights/venv/bin/python /home/pi/lights/Client.py $LIGHTSIDENTITY &
 autossh -R lights-$LIGHTSIDENTITY.serveo.net:80:localhost:3254 serveo.net &
 ruby /home/pi/lights/webhook.rb &
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 sudo systemctl stop lights
 sudo cp lights.service /etc/systemd/system/lights.service
+sudo cp gh-webhook.service /etc/systemd/system/gh-webhook.service
 sudo systemctl daemon-reload
 sudo systemctl start lights
