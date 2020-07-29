2ed22c88558e12f80edc9a861be605f51b87af7f
Tristan Colby <tristan.colb@gmail.com>
2020-07-29 06:51:51 +0100

    Adding config

---
+++ Config.py
@@ -0,0 +1,14 @@
+import Secret
+
+mqtt_ip = Secret.mqtt_ip
+mqtt_username = Secret.mqtt_username
+mqtt_password = Secret.mqtt_password
+healthcheck_identifier = "healthyuwu"
+light_identifier = "whatupdawg"
+mqtt_base_path = "lights/from"
+max_brightness = 40
+ease_step = 0.10
+ease_wait = 0.008
+healthcheck_threshold = 60
+healthcheck_send_period = 30
+unhealthy_blink_cooldown = 15
